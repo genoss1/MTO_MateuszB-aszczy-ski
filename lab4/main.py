e194dec54af109217b1155027b7ddd6588dd56d2
Mateusz Błaszczyński <73279621+genoss1@users.noreply.github.com>
2023-03-27 17:02:17 +0200

    implement #g and write first test

--- lab4/main.py
+++ lab4/main.py
@@ -7,8 +7,12 @@ def my_printf(format_string,param):
     for idx in range(0,len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == 'g':
-                print(param,end="")
-                shouldDo=False
+                if param.isdigit:
+                    reversed_str = str(param)[::-1]
+                    print(reversed_str, end='')
+                    shouldDo=False
+                else:
+                    print(format_string[idx],end="")
             else:
                 print(format_string[idx],end="")
         else:

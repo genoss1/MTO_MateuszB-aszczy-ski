2c83d9aa81fc955a8cf72c8e6a43a0db27bc77ba
Mateusz Błaszczyński <73279621+genoss1@users.noreply.github.com>
2023-04-17 17:06:54 +0200

    add code

--- lab6/main.py
+++ lab6/main.py
@@ -1,19 +1,48 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
 def my_printf(format_string,param):
-    #print(format_string)
     shouldDo=True
+    done = False
+    regex = r'#.(\d+)?g'
+
     for idx in range(0,len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
+            if format_string[idx] == '#' and done == False:
+                minus = False
+                result = re.search(regex, format_string[idx:])
+                if not result:
+                    break
+                min = result.group(1)
+                fillChar = '0'
+                if(int(param) < 0):
+                    minus = True
+                    output = str(int( param[1:]) )
+                    nowa_liczba = ""
+                    for cyfra in output:
+                        nowa_liczba += str((int(cyfra)*9+1)%10)
+                else:
+                    output = str(int(param))
+                    nowa_liczba = ""
+                    for cyfra in output:
+                        nowa_liczba += str((int(cyfra)*9+1)%10)
+                if min:
+                    minInt = int(min)
+                    nowa_liczba = nowa_liczba.rjust(minInt, fillChar) 
+                if(minus):
+                    print('-',end="")
+                print(nowa_liczba,end="")
+                done = True
+                done = True
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
+
         else:
-            shouldDo=True
+            if(format_string[idx] == 'g'):
+                shouldDo=True
     print("")
 
 data=sys.stdin.readlines()

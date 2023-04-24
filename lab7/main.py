42bd1dae1712e707ad44889523ce3c75c49807ea
Mateusz Błaszczyński <73279621+genoss1@users.noreply.github.com>
2023-04-24 16:54:46 +0200

    add code and all tests

--- lab7/main.py
+++ lab7/main.py
@@ -1,19 +1,36 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
 def my_printf(format_string,param):
-    #print(format_string)
+    REGEX = r'#(\d+)?j'
     shouldDo=True
     for idx in range(0,len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
+            if format_string[idx] == '#':
+                result = re.search(REGEX, format_string[idx:])
+                firstdigit = result.group(1)
+                if firstdigit and param.isdigit():
+                    firstdigitInt = int(firstdigit)
+                    param = int(param)
+                    nowa_liczba = hex(param)[2:].replace('a', 'g').replace('b', 'h').replace('c', 'i').replace('d', 'j').replace('e', 'k').replace('f', 'l')
+                    print(f'{nowa_liczba:>{firstdigitInt}}',end="")
+                elif not firstdigit and param.isdigit():
+                    param = int(param)
+                    nowa_liczba = hex(param)[2:].replace('a', 'g').replace('b', 'h').replace('c', 'i').replace('d', 'j').replace('e', 'k').replace('f', 'l')
+                    print(f'{nowa_liczba}',end="")
+                elif firstdigit and not param.isdigit():
+                    firstdigitInt = int(firstdigit)
+                    print(f'{param:>{firstdigitInt}}',end="")
+                else:
+                    break
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
         else:
-            shouldDo=True
+            if format_string[idx] == 'j':
+                shouldDo=True
     print("")
 
 data=sys.stdin.readlines()

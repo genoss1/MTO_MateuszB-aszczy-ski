a44eab3db7020044ab02b4f84725aca60f63f3b4
Mateusz Błaszczyński <73279621+genoss1@users.noreply.github.com>
2023-05-15 12:37:03 +0200

    Implementacja testów jednostkowych z wykorzystaniem nadrzędnego pliku Makefile

--- lab2/main.py
+++ lab2/main.py
@@ -2,24 +2,51 @@
 
 import sys
 
+
+def separateStr(format_string, param):
+    for i in range(0, len(format_string) - 1):
+        if format_string[i] == '#':
+            if format_string[i + 1] == 'k': 
+                return 1, format_string[0:i], -1, format_string[i + 2:len(format_string)]
+            if format_string[i + 1] == '.':
+            	if format_string[i + 2].isnumeric() == False:
+            	    continue
+            	else:
+            	    save = 0
+            	    for j in range(i + 2, len(format_string)):
+            	        if format_string[j].isnumeric() == False:
+            	            save = j
+            	            break
+            	            
+            	    if format_string[save] != 'k':
+            	        continue
+            	    return 2, format_string[0:i], int(format_string[i + 2:save]), format_string[save + 1:len(format_string)]        
+       
+    return 0, format_string, -1, -1    
+
 def my_printf(format_string,param):
-    num = len(format_string)
-    idx = 0
-    while idx < num:
-        if format_string[idx] == '#' and format_string[idx+1] == '.' and format_string[idx+2].isdigit and format_string[idx+3] == 'k':
-            length = int(format_string[idx+2])
-            param = param.swapcase()
-            print(param[0:int(format_string[i+2])], end="")
-            idx = idx + 4
-        elif format_string[idx] == '#' and format_string[idx+1] == 'k':
-            print(param.swapcase(),end="")
-            idx = idx + 2
-        else:
-            print(format_string[idx],end="")
-            idx = idx + 1
-    print("")
+    #print(format_string)
+    typeOf, startFormat, number, endFormat = separateStr(format_string, param)
+    if typeOf == 0:
+    	print(format_string)
+    elif typeOf == 1:
+        print(startFormat, end="")
+        print(param.swapcase(), end="")
+        print(endFormat)
+    else:
+    	print(startFormat, end="")
+    	for i in range(0, min(len(param), number)):
+        	print(param[i].swapcase(), end="")
+    	print(endFormat)
 
 data=sys.stdin.readlines()
 
 for i in range(0,len(data),2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
+   my_printf(data[i].rstrip(),data[i+1].rstrip())
+  
+# my_printf("test", "ignore")
+# my_printf("AA#kAA", "ignORe")
+# my_printf("A#.0kA", "ignore")
+# my_printf("--#.100K", "ignore")
+# my_printf("#100k", "ignore")
+

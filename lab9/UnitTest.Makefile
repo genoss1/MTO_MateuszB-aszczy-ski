69d9351ecb014c2c7a3d4d475eedd05bc3c934c5
Mateusz Błaszczyński <73279621+genoss1@users.noreply.github.com>
2023-05-22 18:15:42 +0200

    add files

--- lab9/UnitTest.Makefile
+++ lab9/UnitTest.Makefile
@@ -64,10 +64,11 @@ endef
 	
 	# perform_test,INPUT_FILE_NAME_WOUT_EXTENSION,TEST_NAME
 	
-	$(call perform_test,noparam,"No param")
-	$(call perform_test,short,Short)
-	$(call perform_test,long,Long)
-	$(call perform_test,bad,Bad)
+	$(call perform_test,nospecifier,"Tests without specifier")
+	$(call perform_test,float,"Float numbers")
+	$(call perform_test,floatLong,"Long Float numbers")
+	$(call perform_test,floatShort,"Short float numbers")
+	$(call perform_test,wrongParam,"Wrong parameter")
 
 	$(call restore_data)
 
@@ -76,10 +77,11 @@ generate:
 
 	# generate_output,INPUT_FILE_NAME_WOUT_EXTENSION
 	
-	$(call generate_answers,noparam)
-	$(call generate_answers,short)
-	$(call generate_answers,long)
-	$(call generate_answers,bad)
+	$(call generate_answers,nospecifier)
+	$(call generate_answers,float)
+	$(call generate_answers,floatLong)
+	$(call generate_answers,floatShort)
+	$(call generate_answers,wrongParam)
 
 	$(call restore_data)
 
@@ -87,7 +89,8 @@ merge:
 	-rm ./input.txt 2>/dev/null
 	-rm ./answers.txt 2>/dev/null
 
-	$(call merge_test,noparam)
-	$(call merge_test,short)
-	$(call merge_test,long)
-	$(call merge_test,bad)
+	$(call merge_test,nospecifier)
+	$(call merge_test,shorfloatt)
+	$(call merge_test,floatLong)
+	$(call merge_test,floatShort)
+	$(call merge_test,wrongParam)
--- lab9/main.py
+++ lab9/main.py
@@ -1,19 +1,95 @@
 #!/usr/bin/env python3
 
 import sys
+import re
+
+def transform(numberString):
+    outputText = []
+    mode = 1;
+    for x in numberString:
+        if x == '.':
+            outputText.append('.')
+            mode = 2;
+        if x == '0':
+            if mode == 1:
+                outputText.append('a')
+            else:
+                outputText.append((0+5)%10)
+        elif x == '1':
+            if mode == 1:
+                outputText.append('b')
+            else:
+                outputText.append((1+5)%10)
+        elif x == '2':
+            if mode == 1:
+                outputText.append('c')
+            else:
+                outputText.append((2+5)%10)
+        elif x == '3':
+            if mode == 1:
+                outputText.append('d')
+            else:
+                outputText.append((3+5)%10)
+        elif x == '4':
+            if mode == 1:
+                outputText.append('e')
+            else:
+                outputText.append((4+5)%10)
+        elif x == '5':
+            if mode == 1:
+                outputText.append('f')
+            else:
+                outputText.append((5+5)%10)
+        elif x == '6':
+            if mode == 1:
+                outputText.append('g')
+            else:
+                outputText.append((6+5)%10)
+        elif x == '7':
+            if mode == 1:
+                outputText.append('h')
+            else:
+                outputText.append((7+5)%10)
+        elif x == '8':
+            if mode == 1:
+                outputText.append('i')
+            else:
+                outputText.append((8+5)%10)
+        elif x == '9':
+            if mode == 1:
+                outputText.append('j')
+            else:
+                outputText.append((9+5)%10)
+    out = ''.join(str(e) for e in outputText)
+    return out.lower()
+
 
 def my_printf(format_string,param):
-    #print(format_string)
     shouldDo=True
+    done = False
+    regex = r'#[.]+?(\d+)?h'
+    floatNum = float(param)
     for idx in range(0,len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
+            if format_string[idx] == '#' and done == False:
+                result = re.search(regex, format_string[idx:])
+                if not result:
+                    print(format_string[idx],end="")
+                    continue
+                
+                precision = result.group(1)
+                if precision:
+                    precisionInt = int(precision)
+                    output = f'{floatNum:.{precisionInt}f}'
+                output = transform(output)
+                print(output,end="")
+                done = True
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
         else:
-            shouldDo=True
+            if format_string[idx] == 'h':
+                shouldDo=True
     print("")
 
 data=sys.stdin.readlines()

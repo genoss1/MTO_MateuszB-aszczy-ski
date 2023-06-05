55681b4a48ba8efb2a7c28dd82a07020de77ff8b
Mateusz Błaszczyński <73279621+genoss1@users.noreply.github.com>
2023-06-05 17:22:12 +0200

    upload lab 10

--- lab11/UnitTest.Makefile
+++ lab11/UnitTest.Makefile
@@ -65,9 +65,11 @@ endef
 	# perform_test,INPUT_FILE_NAME_WOUT_EXTENSION,TEST_NAME
 	
 	$(call perform_test,noparam,"No param")
-	$(call perform_test,short,Short)
+	$(call perform_test,small,Small)
+	$(call perform_test,large,Large)
 	$(call perform_test,long,Long)
-	$(call perform_test,bad,Bad)
+	$(call perform_test,letters,Letters)
+	$(call perform_test,decimal,Decimal)
 
 	$(call restore_data)
 
@@ -77,9 +79,12 @@ generate:
 	# generate_output,INPUT_FILE_NAME_WOUT_EXTENSION
 	
 	$(call generate_answers,noparam)
-	$(call generate_answers,short)
+	$(call generate_answers,small)
+	$(call generate_answers,large)
 	$(call generate_answers,long)
-	$(call generate_answers,bad)
+	$(call generate_answers,letters)
+	$(call generate_answers,decimal)
+	
 
 	$(call restore_data)
 
@@ -88,6 +93,8 @@ merge:
 	-rm ./answers.txt 2>/dev/null
 
 	$(call merge_test,noparam)
-	$(call merge_test,short)
+	$(call merge_test,small)
+	$(call merge_test,large)
 	$(call merge_test,long)
-	$(call merge_test,bad)
+	$(call merge_test,letters)
+	$(call merge_test,decimal)
--- lab11/main.py
+++ lab11/main.py
@@ -2,21 +2,32 @@
 
 import sys
 
-def my_printf(format_string,param):
-    #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
-            else:
-                print(format_string[idx],end="")
-        else:
-            shouldDo=True
-    print("")
+def new_number(old_number):
+    new_number = int((old_number * 2) / len(str(old_number)))
+    if new_number % 2 != 0:
+        return str(hex(new_number)).replace('0x', '')
+    return str(new_number)
 
-data=sys.stdin.readlines()
 
-for i in range(0,len(data),2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
+def my_printf(format_string, param):
+    if '#a' not in format_string:
+        print(format_string)
+        return
+
+    replace = '#a'
+
+    try:
+        param = int(param)
+    except Exception:
+        param = 0
+
+    replace_with = new_number(param)
+
+    print(format_string.replace(replace, replace_with))
+
+
+data = sys.stdin.readlines()
+
+
+for i in range(0, len(data), 2):
+    my_printf(data[i].rstrip(), data[ i + 1].rstrip())
